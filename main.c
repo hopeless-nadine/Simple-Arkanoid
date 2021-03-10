26b13464d7a967231e5e5cc96f9b880e2d7b5431
Nadezhda Solodova <solodova.nadezhda.a@yandex.ru>
2021-03-10 12:40:23 +0300

    Create Margin

--- main.c
+++ main.c
@@ -5,16 +5,28 @@
 #define WIDTH 65
 #define HEIGHT 25
 
-char array[WIDTH][HEIGHT];
+char array[HEIGHT][WIDTH + 1];
 
 void init() {
     for(int i = 0; i < WIDTH; i++) {
         array[0][i] = '#';
     }
+    array[0][WIDTH] = '\0';
+    strncpy(array[1], array[0], WIDTH + 1);
+    for(int i = 1; i < WIDTH - 1; i++) {
+        array[1][i] = ' ';
+    }
+    for(int i = 2; i < HEIGHT; i++) {
+        strncpy(array[i], array[1], WIDTH + 1);
+    }
+
+
 }
 
 void show() {
-    printf("%s", array[0]);
+	for(int i = 0; i < HEIGHT; i++) {
+    	printf("%s\n", array[i]);
+    }
 }
 
 int main(void) {
