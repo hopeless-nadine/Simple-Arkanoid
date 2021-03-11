f86898f7033eaf9ab0743404094192a004d73519
Nadezhda Solodova <solodova.nadezhda.a@yandex.ru>
2021-03-11 17:40:51 +0300

    Added racket

--- main.c
+++ main.c
@@ -5,7 +5,28 @@
 #define WIDTH 65
 #define HEIGHT 25
 
+typedef struct {
+    int x, y;
+    int w;
+} TRacket;
+
+TRacket racket;
 char array[HEIGHT][WIDTH + 1];
+
+void initRacket() {
+    racket.w = 7;
+    racket.x = (WIDTH - racket.w)/2;
+    racket.y = HEIGHT - 1;
+}
+
+void putRacket() {
+    for(int i = racket.x; i < racket.x + racket.w; i++) {
+        array[racket.y][i] = '@';
+    }
+}
+
+
+
 
 void init() {
     for(int i = 0; i < WIDTH; i++) {
@@ -29,8 +50,23 @@ void show() {
     }
 }
 
+void moveRacket(int x) {
+    racket.x = x;
+    if(racket.x < 1) {
+        racket.x = 1;
+        return;
+    }
+    if(racket.x >= WIDTH - racket.w) {
+        racket.x = WIDTH - 1 - racket.w;
+        return;
+    }
+}
+
+
 int main(void) {
+    initRacket();
     init();
+    putRacket();
     show();
     return 0;
 }
