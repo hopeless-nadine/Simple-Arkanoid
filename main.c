38400d58b8a9ee6079367adba45d3b085153b536
Nadezhda Solodova <solodova.nadezhda.a@yandex.ru>
2021-03-24 15:30:09 +0300

    Indluded ncurses, fixed field

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "curses.h"
 
-#define WIDTH 65
-#define HEIGHT 25
+void finish();
 
 typedef struct {
     int x, y;
@@ -11,62 +11,82 @@ typedef struct {
 } TRacket;
 
 TRacket racket;
-char array[HEIGHT][WIDTH + 1];
+char **field;
+
+void init() {
+    initscr();
+    savetty();
+    nonl();
+    cbreak();
+    noecho();
+    timeout(0);
+    leaveok(stdscr, TRUE);
+    curs_set(0);
+}
 
 void initRacket() {
     racket.w = 7;
-    racket.x = (WIDTH - racket.w)/2;
-    racket.y = HEIGHT - 1;
+    racket.x = (COLS - racket.w)/2;
+    racket.y = LINES - 2;
 }
 
 void putRacket() {
     for(int i = racket.x; i < racket.x + racket.w; i++) {
-        array[racket.y][i] = '@';
+        field[racket.y][i] = '@';
     }
 }
 
 
-
-
-void init() {
-    for(int i = 0; i < WIDTH; i++) {
-        array[0][i] = '#';
-    }
-    array[0][WIDTH] = '\0';
-    strncpy(array[1], array[0], WIDTH + 1);
-    for(int i = 1; i < WIDTH - 1; i++) {
-        array[1][i] = ' ';
-    }
-    for(int i = 2; i < HEIGHT; i++) {
-        strncpy(array[i], array[1], WIDTH + 1);
+void initField() {
+    field = (char**) malloc(LINES * sizeof(char*));
+    if(!field) {
+        finish();
     }
 
-
+    for(int i = 0; i < COLS; i++) {
+        field[i] = (char*) malloc(COLS * sizeof(char));
+        if(!field[i]) {
+            finish();
+        }
+    }
+    for(int i = 0; i < COLS; i++) {
+        field[0][i] = '#';
+    }
+    strncpy(field[1], field[0], COLS);
+    for(int i = 1; i < COLS - 1; i++) {
+        field[1][i] = ' ';
+    }
+    for(int i = 2; i < LINES; i++) {
+        strncpy(field[i], field[1], COLS);
+    }
 }
 
+
+void finish() {
+	printf("\n");
+    curs_set(1);
+    clear();
+    refresh();
+    resetty();
+    endwin();
+    exit(0);
+
+}
 void show() {
-	for(int i = 0; i < HEIGHT; i++) {
-    	printf("%s\n", array[i]);
+	for(int i = 0; i < LINES; i++) {
+		for(int j = 0; j < COLS; j++) {
+    		printf("%c", field[i][j]);
+    	}
     }
 }
-
-void moveRacket(int x) {
-    racket.x = x;
-    if(racket.x < 1) {
-        racket.x = 1;
-        return;
-    }
-    if(racket.x >= WIDTH - racket.w) {
-        racket.x = WIDTH - 1 - racket.w;
-        return;
-    }
-}
-
 
 int main(void) {
-    initRacket();
     init();
+    initField();
+    initRacket();
     putRacket();
     show();
+    napms(10000);
+    finish();
     return 0;
 }
